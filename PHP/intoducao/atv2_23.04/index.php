a730ea39b1e2e3c87ee5cab32903ee14c52628ec
otavionegrine_star <otavio.negrine@aluno.senai.br>
2026-04-23 11:17:31 -0300

    atv2

---
+++ PHP/intoducao/atv2_23.04/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="https://revistapesquisa.fapesp.br/wp-content/uploads/2022/01/SITE_BaleiasJubarte-0-1140.jpg" alt="imagem de uma jubarte" >
+
+    <?php
+    //o php se inicia aqui
+
+echo '<h2>Nossa empresa que cuia de baleias<h2>';
+echo 'AMAMOS ELAS 🐳🐳'
+// e ele termina aqui😊😊
+?>
+</body>
+</html>
