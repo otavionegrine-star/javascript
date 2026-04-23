da5c23154846b6d7f004190da6772fca90660f25
otavionegrine_star <otavio.negrine@aluno.senai.br>
2026-04-23 10:51:51 -0300

    aprendendo php

---
+++ PHP/intoducao/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+echo 'mensagem 1 😊😊';
+echo '<h2>mensagem 2🌟🌟<h2>';
+echo 'mensgem 3 🪄🪄'
+
+?>
+</body>
+</html>
